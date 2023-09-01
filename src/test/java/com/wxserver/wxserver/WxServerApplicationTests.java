7145e6b136c8921bb2d2dc1765347fe811e2d6e8
TxpGit <1101642762@qq.com>
2023-09-01 22:09:13 +0800

    微信小程序后端服务第一版

---
+++ src/test/java/com/wxserver/wxserver/WxServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxserver.wxserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
